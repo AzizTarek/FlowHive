1ce8afa0a81952292c32e1cd56b12c6113d6d641
Alexandros <77010224+AzizTarek@users.noreply.github.com>
2024-04-10 22:04:02 +0100

    dashboard

--- trello-clone/app/(platform)/(dashboard)/_components/sidebar.tsx
+++ trello-clone/app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -9,6 +9,7 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Accordion } from "@/components/ui/accordion";
+import { NavItem, Organization } from "./nav-tem";
 
 interface SidebarProps {
   storageKey?: string;
@@ -45,7 +46,6 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
       [id]: !expanded[id],
     }));
   };
-
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
       <>
@@ -75,7 +75,13 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
         className="space-y-2"
       >
         {userMemberships.data.map(({ organization }) => (
-          <p key={organization.id}> {organization.id} </p>
+          <NavItem
+            key={organization.id}
+            isActive={activeOrganization?.id === organization.id}
+            isExpanded={expanded[organization.id]}
+            organization={organization as Organization}
+            onExpand={onExpand}
+          />
         ))}
       </Accordion>
     </>
